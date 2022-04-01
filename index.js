d1ca4e199b3b21f6c6bbb47277be405597cba76f
Jonny Ortiz <j0nny.0rt1z@yahoo.com>
2022-04-01 18:44:39 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+document.querySelector('#main').remove();
+// "Make sure you create an <h1> with id 'victory'").eql('H1')
+const newHeader = document.createElement('h1');
+newHeader.setAttribute('id', 'victory');
+newHeader.textContent = 'Jonny is the champion';
+document.body.append(newHeader);
